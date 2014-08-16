1686acd3304692cd22d1fb4409b0d69853d81347
Helmut Wollmersdorfer <helmut@wollmersdorfer.at>
2014-08-16 05:52:21 +0000

    Import of WOLLMERS/Set-Similarity-CosinePP-0.009 from CPAN.
    gitpan-cpan-distribution: Set-Similarity-CosinePP
    gitpan-cpan-version:      0.009
    gitpan-cpan-path:         WOLLMERS/Set-Similarity-CosinePP-0.009.tar.gz
    gitpan-cpan-author:       WOLLMERS
    gitpan-cpan-maturity:     released

--- lib/Set/Similarity/CosinePP.pm
+++ lib/Set/Similarity/CosinePP.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use parent 'Set::Similarity';
 
-our $VERSION = '0.008';
+our $VERSION = '0.009';
 
 sub from_sets {
   my ($self, $set1, $set2) = @_;
@@ -81,7 +81,12 @@ __END__
 
 =head1 NAME
 
-Set::Similarity::Cosine - Cosine similarity for sets
+Set::Similarity::CosinePP - Cosine similarity for sets pure Perl vector implementation
+
+=for html
+
+<a href="https://travis-ci.org/wollmers/Set-Similarity-CosinePP"><img src="https://travis-ci.org/wollmers/Set-Similarity-CosinePP.png" alt="Set-Similarity-CosinePP"></a>
+<a href='https://coveralls.io/r/wollmers/Set-Similarity-CosinePP?branch=master'><img src='https://coveralls.io/repos/wollmers/Set-Similarity-CosinePP/badge.png?branch=master' alt='Coverage Status' /></a>
 
 =head1 SYNOPSIS
 
